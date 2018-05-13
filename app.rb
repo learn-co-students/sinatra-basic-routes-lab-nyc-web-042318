693a8af7bda6fa6ec81e1c895dcb90b0e8b20d9f
ensallee <ensallee@gmail.com>
2018-05-13 15:12:43 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World"
+  end
+
+  get '/name' do
+    "My name is Betsy"
+  end
+
+  get '/hometown' do
+    "My hometown is Kansas City"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Hallelujah by Rufus Wainwright"
+  end
+
 end
