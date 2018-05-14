c8e36886451091d4a2e6f3da2679874c794d9082
Derek Chen <derekgc@icloud.com>
2018-05-14 09:50:38 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+
+  get '/hometown' do
+    "My hometown is "
+  end
+
+  get '/name' do
+    "My name is "
+  end
+
+  get '/favorite-song' do
+    "My favorite song is "
+  end
+
+
 end
